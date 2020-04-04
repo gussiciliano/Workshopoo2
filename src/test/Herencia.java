5948b9c2ebf677cf0da87d86d4b148809cf90697
Gustavo Siciliano <gussiciliano@gmail.com>
2020-04-04 18:37:33 -0300

    prueba

--- src/test/Herencia.java
+++ src/test/Herencia.java
@@ -8,7 +8,11 @@ public class Herencia {
 	public static void main(String[] args) {
 		Persona dev = new Programador("ape", "nom", 23, 23);
 		dev.calcularSueldo();
-		Persona qa = new Tester("ape", "nom", 23, 23, 10);
-		qa.calcularSueldo();
+		Persona qa1 = new Tester("ape", "nom", 23, 23, 10);
+		qa1.calcularSueldo();
+		Persona qa2 = new Tester("ape", "nom", 23, 23, 10);
+		qa2.calcularSueldo();
+		Persona qa3 = new Tester("ape", "nom", 23, 23, 10);
+		qa3.calcularSueldo();
 	}
 }
